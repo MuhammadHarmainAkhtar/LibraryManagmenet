c397765cb04672323ca685f6da0e03f2255ac67a
MrHarmain <mrharmain9@gmail.com>
2025-01-16 07:23:53 +0500

    Book page Completed...

--- my-app/components/BookList.tsx
+++ my-app/components/BookList.tsx
@@ -7,13 +7,16 @@ interface Props {
   containerClassName?: string;
 }
 const BookList = ({ title, books, containerClassName }: Props) => {
+  if(books.length < 2) return;
   return (
     <section className={containerClassName}>
       <h1 className="font-bebas-neue text-4xl text-light-100">{title}</h1>
 
-      <ul className="book-list">{books.map((book)=>(
-        <BookCard key={book.title} {...book}/>
-      ))}</ul>
+      <ul className="book-list">
+        {books.map((book) => (
+          <BookCard key={book.title} {...book} />
+        ))}
+      </ul>
     </section>
   );
 };
